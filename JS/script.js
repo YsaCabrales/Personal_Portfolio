ca3e2095f18d4fcbcdd7229b6047cbe73840743a
regelcabrales <regelcabrales@gmail.com>
2024-04-10 17:06:14 +0800

    Styled skills section

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 const myRoles = document.querySelector('#my-roles p');
-
+const acc = document.getElementsByClassName("accordion");
 const roles = [
     'full stack developer',
     'graphic designer',
@@ -29,5 +29,24 @@ document.querySelector('#my-roles').addEventListener('click', () =>{
 function scrollToDiv(divId) {
     const targetElement = document.getElementById(`${divId}`);
     targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    let panel = this.nextElementSibling;
+    const accIcon = this.getElementsByTagName('div')[0];
+    console.log(this);
+    console.log(accIcon);
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      accIcon.style.transform = "rotate(0deg)";
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      accIcon.style.transform = "rotate(90deg)";
+    }
+    
+  });
 }
   
