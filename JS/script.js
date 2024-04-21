a2433f50837ea2560bc6e58d0ef54f9b9df132d8
regelcabrales <regelcabrales@gmail.com>
2024-04-21 13:45:17 +0800

    Added scroll to top button

--- JS/script.js
+++ JS/script.js
@@ -79,3 +79,22 @@ function next() {
 function prev() {
   slider.scrollLeft -= cardWidth;
 }
+
+// Get the button
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
